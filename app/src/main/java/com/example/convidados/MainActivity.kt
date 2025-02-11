8e95e6a9c53248e4d095b64531bd1654a8bea525
WRamos21 <ramossantoswesley@gmail.com>
2025-02-11 18:45:19 -0300

    Modificando menu de configuração e lateral
    Retirando menu de configurações e modificando o menu lateral para mostrar "Todos os convidados", "Presentes", "Ausentes"

--- app/src/main/java/com/example/convidados/MainActivity.kt
+++ app/src/main/java/com/example/convidados/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.convidados
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -15,40 +18,54 @@ import com.example.convidados.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+        /* 1.0 Float action button
+        Este botão é resposavel por abrir uma nova activity, para isso foi criado
+        em UI a Activity GuestForm com o layout Fragmente all guests
+        2.0 Modificando a navigation bar
+        dentro da navigation bar agora existe o botão "todos os convidados", para isso substituimos
+        o arquivo kotlin HomeFragment e sua view model para AllGuests, assim como o xml.
+        Dentro de Menu em activity_main_drawer tems todos os itens de navegação, incluindo home que
+        será alterado, podemos modficar o texto para "todos os convidados" e o id para nav_all_guests.
+        podemos também alterar o titulo da activity utilizando o campo label em activity_main_drawer.
+        mas para que possamos navegar para essa fragment (renomeada para fragment_all_guests)
+        temos que modificar o id em mobile_navigation também, lembrando que aqui na MainActivity
+        nos referenciamos as fragments em AppBarConfigure portando é necessario atualizar a referencia.
+
+         */
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null)
-                    .setAnchorView(R.id.fab).show()
+            startActivity(Intent(applicationContext, GuestFormActivity::class.java))
         }
+        setupNavigation()
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigation() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_all_guests, R.id.nav_absent, R.id.nav_present
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/convidados/ui/AbsentFragment.kt
+++ app/src/main/java/com/example/convidados/ui/AbsentFragment.kt
@@ -1,4 +1,4 @@
-package com.example.convidados.ui.gallery
+package com.example.convidados.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.convidados.databinding.FragmentGalleryBinding
+import com.example.convidados.databinding.FragmentAbsentBinding
 
-class GalleryFragment : Fragment() {
+class AbsentFragment : Fragment() {
 
-private var _binding: FragmentGalleryBinding? = null
+private var _binding: FragmentAbsentBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -21,14 +21,14 @@ private var _binding: FragmentGalleryBinding? = null
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+    val ViewModel =
+            ViewModelProvider(this).get(AbsentViewModel::class.java)
 
-    _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+    _binding = FragmentAbsentBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textGallery
-    galleryViewModel.text.observe(viewLifecycleOwner) {
+    val textView: TextView = binding.textSlideshow
+    ViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
     return root
