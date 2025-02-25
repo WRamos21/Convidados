4d52ed739ad29b59362d51e792ec5171e50e77ba
WRamos21 <ramossantoswesley@gmail.com>
2025-02-25 18:55:37 -0300

    Configuração gradle para receber ROOM e criação de entidade

--- app/src/main/java/com/example/convidados/model/GuestModel.kt
+++ app/src/main/java/com/example/convidados/model/GuestModel.kt
@@ -1,5 +1,30 @@
 package com.example.convidados.model
 
-data class GuestModel(val id: Int, val name: String, var presence: Boolean) {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/* Implementando o ROOM
+Utilizamos @ para implementar um anotation para colocar atributos e configurações a mais na classe
+Basicamnete colocando Entity digo que a guestModel é um entidade, siginifica que ela será salva no
+banco de dados, de forma que o ROOM vai ser que precisa persisitir GuestModel no banco
+- Desta forma não precisamos mais se preocupar com a criação da tabela lá me GuestDataBase
+- Preciso criar a anotation para marcar para o ROOM como colunas usando @ColumnInfo e @PrymaryKey
+para identificar uma coluna como chave primaria
+
+ */
+
+@Entity(tableName = "Guest")
+class GuestModel {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0
+
+    @ColumnInfo(name = "name")
+    val name: String = ""
+
+    @ColumnInfo(name = "presence")
+    var presence: Boolean = false
 
 }
